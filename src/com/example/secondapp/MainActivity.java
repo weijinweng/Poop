cbe89b2ba8d0a9808b3bb70656f6d0e5cde6562a
weijinweng <weijin22@hotmail.com>
2014-04-11 04:05:08 -0500

    Update

--- src/com/example/secondapp/MainActivity.java
+++ src/com/example/secondapp/MainActivity.java
@@ -1,45 +1,35 @@
 package com.example.secondapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-
-public class MainActivity extends Activity implements OnClickListener {
-	
-	private final String TAG = "MainActivity";
-	private Button buttonTweet;
-	private EditText editStatus;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-
-		
-		buttonTweet = (Button) findViewById(R.id.tweetButton);
-		editStatus = (EditText) findViewById(R.id.editStatus);
-		
-		buttonTweet.setOnClickListener(this);
-		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		String status = editStatus.getText().toString();
-		Log.d(TAG, "Click with tweet" + status);
-	}
-
+public class MainActivity extends Activity implements
+OnClickListener { //
+private static final String TAG = "StatusActivity";
+private EditText editStatus;
+private Button buttonTweet;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	editStatus = (EditText) findViewById(R.id.editStatus);
+	buttonTweet = (Button) findViewById(R.id.buttonTweet);
+	buttonTweet.setOnClickListener(this); //
+}
+@Override
+public void onClick(View view) { //
+String status = editStatus.getText().toString(); //
+Log.d(TAG, "onClicked with status: " + status); //
+}
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+getMenuInflater().inflate(R.menu.main, menu);
+return true;
+}
 }
